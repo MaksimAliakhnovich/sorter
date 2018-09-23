b02090362dc695449a2cc52e68943f5cd0289234
Maksim <33697594+MaksimAliakhnovich@users.noreply.github.com>
2018-09-23 23:41:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,60 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.mainArray = [];
+    this.compareFunction;
+    this.compareFunctionName = "NUMERIC_COMPARATOR";
   }
 
   add(element) {
-    // your implementation
+    this.mainArray.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.mainArray[index];
   }
 
   get length() {
-    // your implementation
+    return this.mainArray.length;
   }
 
   toArray() {
-    // your implementation
+    return this.mainArray;
   }
 
   sort(indices) {
-    // your implementation
+
+    const NUMERIC_COMPARATOR = (left, right) => left - right; // компаратор для сравнения чисел
+    let sortedTmpArray = []; // временный массив чисел для сортировки
+    let sortedIndices = indices.sort(NUMERIC_COMPARATOR); // массив отсортированных индексов
+
+    /* Во временный массив заносим элементы уже добавленные элементы
+    с запрашиваемыми индексами*/
+    for(let i = 0; i < sortedIndices.length; i++) {
+      sortedTmpArray.push(this.at(sortedIndices[i]));
+    }
+
+    /* Выбор сортировки */
+    switch (this.compareFunctionName) {
+      case "NUMERIC_COMPARATOR":        
+        sortedTmpArray.sort(NUMERIC_COMPARATOR);
+        break;
+      case this.compareFunctionName:
+        sortedTmpArray.sort(this.compareFunction);
+        break;
+      default:        
+        sortedTmpArray.sort(NUMERIC_COMPARATOR); // на всякий случай
+    }  
+
+    /* Замена не сортированных элементов в основном массиве отсортированными
+    из временного массива */
+    for(let n = 0; n < sortedIndices.length; n++) {
+      this.mainArray.splice(sortedIndices[n], 1, sortedTmpArray[n]);
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
+    this.compareFunctionName = compareFunction.name;
   }
 }
 
